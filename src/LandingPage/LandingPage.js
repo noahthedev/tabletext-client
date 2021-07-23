c1a3679a70f7355837359599a773e317cc167253
Noah Roberts <noahmroberts@gmail.com>
2021-07-23 16:58:08 -0500

    add landing page

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class LandingPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>TableText</h1>
+        
+      </div>
+    )
+  }
+}

661df9a107d251a79900193809272d83588618af
Noah Roberts <noahmroberts@gmail.com>
2021-08-03 14:03:43 -0500

    updated phone input

--- src/CreateGuest/CreateGuest.js
+++ src/CreateGuest/CreateGuest.js
@@ -2,8 +2,19 @@ import React from 'react'
 import './CreateGuest.css'
 import { config } from '../config'
 import ApiContext from '../ApiContext'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 export default class CreateGuest extends React.Component {
+  state = {
+    phoneNumber: ''
+  }
+
+  updateNumber = (num) => {
+    this.setState({
+      phoneNumber: num
+    })
+  }
 
   static contextType = ApiContext
 
@@ -12,7 +23,7 @@ export default class CreateGuest extends React.Component {
     const newGuest = {
       guestname: e.target['guest-name'].value,
       guestcount: e.target['guest-count'].value,
-      phone: e.target['guest-phone'].value
+      phone: this.state.phoneNumber
     }
     fetch(`${config.API_ENDPOINT}/waitlist`, {
       method: 'POST',
@@ -42,15 +53,19 @@ export default class CreateGuest extends React.Component {
         onSubmit={this.handleSubmit}>
         <div>
             <label htmlFor='name-input'>Name</label>
-            <input type='text' id='name-input' name='guest-name' required/>
+            <input type='text' id='name-input' name='guest-name' required autoFocus/>
           </div>
           <div>
             <label htmlFor='count-input'>Count</label>
             <input type='number' id='count-input' name='guest-count' required/>
           </div>
-          <div>
-            <label htmlFor='phone-input'>Phone Number</label>
-            <input type='text' id='phone-input' name='guest-phone' required/>
+          <div className='phone-input'>
+            <label htmlFor='PhoneInput'>Phone Number</label>
+          <PhoneInput
+            defaultCountry="US" 
+            value={undefined}
+            onChange={this.updateNumber}
+          />
           </div>
           <div className='button-container'>
           <button type='submit'>Add Guest</button>
